f03289f397b70aec7e922be87c99207f1b81ac68
VIGNESH <nvignesh397@gmail.com>
2023-12-07 08:08:41 +0530

    First Commit

---
+++ lib/providers/user_places.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+import 'package:favorite_places_app/models/place.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class UserPlacesNotifer extends StateNotifier<List<Place>> {
+  UserPlacesNotifer() : super(const []);
+
+  void addPlace(String title, File image) {
+    final newPlace = Place(title: title, image: image);
+    state = [newPlace, ...state];
+  }
+}
+
+final userPlacesProvider =
+    StateNotifierProvider<UserPlacesNotifer, List<Place>>(
+        (ref) => UserPlacesNotifer());
